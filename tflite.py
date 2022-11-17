c19fe82f1c1bd4ac760d328ab6ba7ec257c8660a
Kyle <kyle@hoffpauir.com>
2022-11-17 14:21:59 -0500

    tflite model created -- tflite model needs to predict on new data

--- tflite.py
+++ tflite.py
@@ -7,19 +7,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import subprocess
+from tensorflow.lite.python.lite import TFLiteConverter
 
-train_df = pd.read_csv('IOT-temp.csv')
-train_df.columns = [column.lower() for column in train_df.columns]
-print(train_df.columns)
+# Load TFLite model and allocate tensors.
+interpreter = tf.lite.Interpreter(model_path="model_quant_tl.tflite")
+#allocate the tensors
+interpreter.allocate_tensors()
 
-test_df = pd.read_csv('IOT-temp.csv')
-test_df.columns = [column.lower() for column in test_df.columns]
-print(test_df.columns)
+#get input and output tensors
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
-data = pd.read_csv('IOT-temp.csv')
-print(data.head())
+# Load MNIST dataset.
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+x_train = x_train.astype(np.float32)
+x_test = x_test.astype(np.float32)
 
-print(train_df.head())
-
-
-# https://www.tensorflow.org/lite/models/convert
+#set the tensor to point to the input data to be inferred
+input_index = interpreter.get_input_details()[0]["index"]
+interpreter.set_tensor(input_index, x_test)
+#Run the inference
+interpreter.invoke()
+output_details = interpreter.get_output_details()
+output_data = interpreter.get_tensor(output_details[0]['index'])
+print(output_data)
