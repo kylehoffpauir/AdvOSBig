a9b8d1f7e3bcbe4f5f852ce082ca0d4da6499391
Kyle <kyle@hoffpauir.com>
2022-11-17 14:42:14 -0500

    tflite working classifier

--- tflite.py
+++ tflite.py
@@ -1,3 +1,5 @@
+import math
+
 import tensorflow as tf
 from tensorflow import keras
 
@@ -9,26 +11,52 @@ import os
 import subprocess
 from tensorflow.lite.python.lite import TFLiteConverter
 
+# Helper function to display digit images
+def show_sample(images, labels, sample_count=25):
+    # Create a square with can fit {sample_count} images
+    grid_count = math.ceil(math.ceil(math.sqrt(sample_count)))
+    grid_count = min(grid_count, len(images), len(labels))
+
+    plt.figure(figsize=(2*grid_count, 2*grid_count))
+    for i in range(sample_count):
+        plt.subplot(grid_count, grid_count, i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(images[i], cmap=plt.cm.gray)
+        plt.xlabel(labels[i])
+    plt.show()
+
+# Download a test image
+zero_img_path = keras.utils.get_file(
+    'zero.png',
+    'https://storage.googleapis.com/khanhlvg-public.appspot.com/digit-classifier/zero.png'
+)
+image = keras.preprocessing.image.load_img(
+    zero_img_path,
+    color_mode = 'grayscale',
+    target_size=(28, 28),
+    interpolation='bilinear'
+)
+
+# Pre-process the image: Adding batch dimension and normalize the pixel value to [0..1]
+# In training, we feed images in a batch to the model to improve training speed, making the model input shape to be (BATCH_SIZE, 28, 28).
+# For inference, we still need to match the input shape with training, so we expand the input dimensions to (1, 28, 28) using np.expand_dims
+input_image = np.expand_dims(np.array(image, dtype=np.float32) / 255.0, 0)
+
+# Show the pre-processed input image
+show_sample(input_image, ['Input Image'], 1)
+
+
+
 # Load TFLite model and allocate tensors.
 interpreter = tf.lite.Interpreter(model_path="model_quant_tl.tflite")
 #allocate the tensors
 interpreter.allocate_tensors()
-
-#get input and output tensors
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
-
-# Load MNIST dataset.
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
-x_train = x_train.astype(np.float32)
-x_test = x_test.astype(np.float32)
-
-#set the tensor to point to the input data to be inferred
-input_index = interpreter.get_input_details()[0]["index"]
-interpreter.set_tensor(input_index, x_test)
-#Run the inference
+interpreter.set_tensor(interpreter.get_input_details()[0]["index"], input_image)
 interpreter.invoke()
-output_details = interpreter.get_output_details()
-output_data = interpreter.get_tensor(output_details[0]['index'])
-print(output_data)
+output = interpreter.tensor(interpreter.get_output_details()[0]["index"])()[0]
+# Print the model's classification result
+digit = np.argmax(output)
+print('Predicted Digit: %d\nConfidence: %f' % (digit, output[digit]))
+
