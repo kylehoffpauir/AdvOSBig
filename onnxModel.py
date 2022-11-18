b06e98eb4ea728596b16cb9142dc974457ea33b0
kolidesa <77902590+kolidesa@users.noreply.github.com>
2022-11-18 13:40:31 -0500

    Working on getting onnx to predict

---
+++ onnxModel.py
@@ -0,0 +1,80 @@
+import math
+
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import subprocess
+import mxnet as mx
+import onnx
+from mxnet import gluon
+from mxnet.gluon import nn
+from mxnet import autograd as ag
+from mxnet.contrib import onnx as onnx_mxnet
+
+# Helper function to display digit images
+def show_sample(images, labels, sample_count=25):
+    # Create a square with can fit {sample_count} images
+    grid_count = math.ceil(math.ceil(math.sqrt(sample_count)))
+    grid_count = min(grid_count, len(images), len(labels))
+
+    plt.figure(figsize=(2*grid_count, 2*grid_count))
+    for i in range(sample_count):
+        plt.subplot(grid_count, grid_count, i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(images[i], cmap=plt.cm.gray)
+        plt.xlabel(labels[i])
+    plt.show()
+
+# Download a test image
+zero_img_path = keras.utils.get_file(
+    'zero.png',
+    'https://storage.googleapis.com/khanhlvg-public.appspot.com/digit-classifier/zero.png'
+)
+image = keras.preprocessing.image.load_img(
+    zero_img_path,
+    color_mode = 'grayscale',
+    target_size=(28, 28),
+    interpolation='bilinear'
+)
+
+# Pre-process the image: Adding batch dimension and normalize the pixel value to [0..1]
+# In training, we feed images in a batch to the model to improve training speed, making the model input shape to be (BATCH_SIZE, 28, 28).
+# For inference, we still need to match the input shape with training, so we expand the input dimensions to (1, 28, 28) using np.expand_dims
+input_image = np.expand_dims(np.array(image, dtype=np.float32) / 255.0, 0)
+
+# Show the pre-processed input image
+show_sample(input_image, ['Input Image'], 1)
+
+onnx_model = onnx.load("converted.onnx")
+print(onnx_model)
+sess = rt.InferenceSession('converted.onnx')
+
+input_name = sess.get_inputs()[0].name
+label_name = sess.get_outputs()[0].name
+pred_onx = sess.run(
+    [label_name], {input_name: X_test.astype(numpy.float32)})[0]
+print(pred_onx)
+
+
+
+'''# Load TFLite model and allocate tensors.
+interpreter = tf.lite.Interpreter(model_path="model_quant_tl.tflite")
+#allocate the tensors
+interpreter.allocate_tensors()
+interpreter.set_tensor(interpreter.get_input_details()[0]["index"], input_image)
+interpreter.invoke()
+output = interpreter.tensor(interpreter.get_output_details()[0]["index"])()[0]'''
+
+
+
+# Print the model's classification result
+digit = np.argmax(output)
+print('Predicted Digit: %d\nConfidence: %f' % (digit, output[digit]))
+
